8644070b609054f85f6239a68f4a2604f1c4f255
sevcak <sevclukas0@gmail.com>
2023-09-04 14:46:08 +0200

    setup: add React Router, set up HashRouter and `/fcc` endpoint`

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/fcc" Component={App}/>
+        <Route path="/" Component={App}/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
